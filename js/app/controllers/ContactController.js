0507128ba1a8cbfcca3e21b586db503a857c8582
dylan.okeefe <dylanokeefe@gmail.com>
2016-04-25 16:05:59 -0400

    done

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Dylan O\'Keefe';
+
+  $timeout(function () {
+    $scope.name = 'Dylan O\'Keefe III';
+  }, 5000);
 }
 
 angular
